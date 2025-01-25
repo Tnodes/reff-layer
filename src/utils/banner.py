65632dbef93b1187d6629f57b0d145519cb126bf
Tnodes <biel08468@gmail.com>
2025-01-26 05:13:11 +0700

    fix(banner): add OS-specific font name handling for cross-platform compatibility

--- src/utils/banner.py
+++ src/utils/banner.py
@@ -1,8 +1,17 @@
 from pyfiglet import Figlet
 from colorama import Fore
+import os
 
 def print_banner():
-    figlet = Figlet(font='Larry3D')
-    print(figlet.renderText('Tdrop'))
-    print(Fore.WHITE + 'Program Name: Layeredge Auto Referral Tool')
-    print(Fore.WHITE + 'Telegram    : ' + Fore.MAGENTA + 'https://t.me/tdropid\n')
+    # Check the operating system
+    if os.name == 'posix':  # Linux and MacOS
+        figlet = Figlet(font='Larry 3D')
+        print(figlet.renderText('Tdrop'))
+            
+        print(Fore.WHITE + 'Program Name: Layeredge Auto Referral Tool')
+        print(Fore.WHITE + 'Telegram    : ' + Fore.MAGENTA + 'https://t.me/tdropid\n')
+    elif os.name == 'nt':  # Windows
+        figlet = Figlet(font='Larry3D')
+        print(figlet.renderText('Tdrop'))
+        print(Fore.WHITE + 'Program Name: Layeredge Auto Referral Tool')
+        print(Fore.WHITE + 'Telegram    : ' + Fore.MAGENTA + 'https://t.me/tdropid\n')
