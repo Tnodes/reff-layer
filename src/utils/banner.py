03371b9bf9d13a0117df03db9ada874dc8b0f186
Tnodes <biel08468@gmail.com>
2025-01-26 05:16:23 +0700

    refactor(banner): simplify banner printing by removing OS-specific logic

--- src/utils/banner.py
+++ src/utils/banner.py
@@ -1,17 +1,8 @@
 from pyfiglet import Figlet
 from colorama import Fore
-import os
 
 def print_banner():
-    # Check the operating system
-    if os.name == 'posix':  # Linux and MacOS
-        figlet = Figlet(font='Larry 3D')
-        print(figlet.renderText('Tdrop'))
-            
-        print(Fore.WHITE + 'Program Name: Layeredge Auto Referral Tool')
-        print(Fore.WHITE + 'Telegram    : ' + Fore.MAGENTA + 'https://t.me/tdropid\n')
-    elif os.name == 'nt':  # Windows
-        figlet = Figlet(font='Larry3D')
-        print(figlet.renderText('Tdrop'))
-        print(Fore.WHITE + 'Program Name: Layeredge Auto Referral Tool')
-        print(Fore.WHITE + 'Telegram    : ' + Fore.MAGENTA + 'https://t.me/tdropid\n')
+    figlet = Figlet(font='larry3d')
+    print(figlet.renderText('Tdrop'))
+    print(Fore.WHITE + 'Program Name: Layeredge Auto Referral Tool')
+    print(Fore.WHITE + 'Telegram    : ' + Fore.MAGENTA + 'https://t.me/tdropid\n')
